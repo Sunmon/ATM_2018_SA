ee1c3afe27e9b9b50ee74575fe1d1dda679a1056
sunmon <blackmilk274@gmail.com>
2018-12-13 15:06:54 +0900

    Initial commit

---
+++ src/view/mainMenu.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.*;
+
+public class mainMenu
+{
+    private JPanel panel1;
+    private JButton 출금Button;
+    private JButton 송금Button;
+    private JButton 예금Button;
+}
