068bd2a631f13a819b8ceeb3f83c8498781ceb33
sunmon <blackmilk274@gmail.com>
2018-12-15 16:06:00 +0900

    add: InsertCard에서 버튼들 누르는대로 숫자 textField에 입력됨. OK버튼은 아직 미구현

--- src/view/InsertCardView.java
+++ src/view/InsertCardView.java
@@ -1,6 +1,9 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class InsertCardView extends JPanel
 {
@@ -23,6 +26,74 @@ public class InsertCardView extends JPanel
     private JButton a0Button;
 
 
+    CardLayout c;
 
-    //TODO: 버튼 누르는대로 위에 textField에 뜨게 하기.
+
+    //TODO: OK버튼 누르면 다음 화면으로 넘어가게 하기
+    public InsertCardView()
+    {
+
+
+        ActionListener listener = new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                addToText(e.getSource(), creditTextField);
+            }
+        };
+
+        //add button listener
+        a1Button.addActionListener(listener);
+        a2Button.addActionListener(listener);
+        a3Button.addActionListener(listener);
+        a4Button.addActionListener(listener);
+        a5Button.addActionListener(listener);
+        a6Button.addActionListener(listener);
+        a7Button.addActionListener(listener);
+        a8Button.addActionListener(listener);
+        a9Button.addActionListener(listener);
+        a0Button.addActionListener(listener);
+        deleteButton.addActionListener(listener);
+        resetButton.addActionListener(listener);
+        okButton.addActionListener(listener);
+    }
+
+
+    //버튼 눌렀을때 textField에 누른 숫자대로 뜨게 하는 메소드
+    public void addToText(Object o, JTextField textField)
+    {
+        //누른 버튼 정보를 but에 숫자로 저장
+        Object[] buttons = new Object[]{a0Button, a1Button, a2Button, a3Button, a4Button, a5Button, a6Button,
+                a7Button, a8Button, a9Button, deleteButton, okButton, resetButton};
+        int but = 0;
+        for(Object b: buttons)
+        {
+            if(o.equals(b)) break;
+            but++;
+        }
+
+
+        //기존 textField의 내용을 갱신
+        String temp = textField.getText();
+        switch(but)
+        {
+            case 10:    //delete
+                if(temp.length() == 0) break;
+                temp = temp.substring(0, temp.length()-1);
+                break;
+            case 11:    //ok
+            case 12:    //reset
+                temp = "";
+                break;
+            default:
+                temp+= Integer.toString(but);
+                break;
+        }
+        textField.setText(temp);
+    }
+
+
+
+
 }
