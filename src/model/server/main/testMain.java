40e1bb3e785eebf7adf5d4a7f8ef9ceecc17a370
3jang <39076276+3jang@users.noreply.github.com>
2018-12-18 15:16:42 +0900

    Update testMain.java

--- src/model/server/main/testMain.java
+++ src/model/server/main/testMain.java
@@ -8,12 +8,13 @@ public class testMain {
         
         // 이런식으로 사용해야 됩니다. 기본적으로 서버에 있는 함수들이 이런구조에요
         database db = new database();
-		db.connect();
-		if(db.isValid("111111111")) {
-			db.getMoney("111111111", "50000");
-			db.putMoney("111111111", "50000");
-		}
-		db.disconnect();
-		db.commit();
+	db.connect();
+	db.rollback();    	
+	if(db.isValid("111111111")) {
+		db.getMoney("111111111", "50000");
+		db.putMoney("111111111", "50000");
+	}
+	db.disconnect();
+	db.commit();
     }
 }
