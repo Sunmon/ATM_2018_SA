de88d75137c754db64fa2d0e7d6d3fa051fc1a06
3jang <39076276+3jang@users.noreply.github.com>
2018-12-18 15:15:35 +0900

    Update testMain.java
    메인수정

--- src/model/server/main/testMain.java
+++ src/model/server/main/testMain.java
@@ -5,9 +5,15 @@ import model.server.DB.database;
 public class testMain {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+        
+        // 이런식으로 사용해야 됩니다. 기본적으로 서버에 있는 함수들이 이런구조에요
         database db = new database();
-        db.connect();
-        db.isValid("111111111");
-        db.getMoney("111111111", "30000");
+		db.connect();
+		if(db.isValid("111111111")) {
+			db.getMoney("111111111", "50000");
+			db.putMoney("111111111", "50000");
+		}
+		db.disconnect();
+		db.commit();
     }
 }
